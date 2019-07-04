60716d086c55a79f00c688eed4a4ea773b63e2df
Bui Van Lam <bui.van.lam@sun-asterisk.com>
2019-07-05 01:10:43 +0700

    header-login

---
+++ resources/views/shared/navbar.blade.php
@@ -0,0 +1,387 @@
+<header id="header" class="full-header dark">
+<div id="header-wrap">
+<div class="container clearfix">
+<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+<div id="logo">
+<a class="standard-logo" data-dark-logo="images/logo-dark.png" href="{{ url('/') }}"></a>
+</div>
+
+<nav id="primary-menu">
+<ul>
+<li><a href="{{ url('/') }}"><div>Home</div></a></li>
+<li class="current"><a href="#"><div>Women's Fashion</div></a>
+<ul>
+<li><a href="#"><div><i class="icon-stack"></i>Váy đầm </div></a></li>
+<li><a href="widgets.html"><div><i class="icon-gift"></i>Áo</div></a>
+<ul>
+<li><a href="widgets.html"><div>Áo Sơ Mi Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Len Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Cardigan Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Thun Nữ</div></a></li>
+</ul>
+</li>
+<li><a href="#"><div><i class="icon-umbrella"></i>ÁO KHOÁC NỮ</div></a>
+<ul>
+<li><a href="header-light.html"><div>Áo Khoác Dạ</div></a></li>
+<li><a href="header-dark.html"><div>Áo Khoác Phao/Lông Vũ</div></a></li>
+<li><a href="header-transparent.html"><div>Áo Khoác Da</div></a></li>
+<li><a href="header-semi-transparent.html"><div>Áo Khoác Kaki</div></a></li>
+<li><a href="header-side-left.html"><div>Áo Khoác Vest Blazer</div></a></li>
+<li><a href="header-side-right.html"><div>Áo Khoác Lông</div></a></li>
+<li><a href="header-floating.html"><div>Áo Khoác Khác</div></a></li>
+</ul>
+</li>
+<li><a href="side-panel.html"><div><i class="icon-line-layout"></i>QUẦN – CHÂN VÁY</div></a>
+<ul>
+<li><a href="side-panel-left-overlay.html"><div>Chân Váy</div></a></li>
+<li><a href="side-panel-left-push.html"><div>Quần Nữ</div></a></li>
+<li><a href="side-panel-right-overlay.html"><div>Quần Jean Nữ</div></a></li>
+</ul>
+</li>
+<li><a href="#"><div><i class="icon-align-justify2"></i>KHÁC</div></a>
+<ul>
+<li><a href="header-light.html"><div>Bộ Đồ Thể Thao</div></a></li>
+<li><a href="menu-2.html"><div>Đồ Bơi Bikini</div></a></li>
+</ul>
+</li>
+<li><a href="profile.html"><div><i class="icon-user"></i>User Profile</div></a></li>
+</ul>
+</li>
+<li class="current"><a href="#"><div>Men's Fashion</div></a>
+<ul>
+<li><a href="#"><div><i class="icon-stack"></i>Váy đầm </div></a></li>
+<li><a href="widgets.html"><div><i class="icon-gift"></i>Áo</div></a>
+<ul>
+<li><a href="widgets.html"><div>Áo Sơ Mi Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Len Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Cardigan Nữ</div></a></li>
+<li><a href="widgets.html"><div>Áo Thun Nữ</div></a></li>
+</ul>
+</li>
+<li><a href="#"><div><i class="icon-umbrella"></i>ÁO KHOÁC NỮ</div></a>
+<ul>
+<li><a href="header-light.html"><div>Áo Khoác Dạ</div></a></li>
+<li><a href="header-dark.html"><div>Áo Khoác Phao/Lông Vũ</div></a></li>
+<li><a href="header-transparent.html"><div>Áo Khoác Da</div></a></li>
+<li><a href="header-semi-transparent.html"><div>Áo Khoác Kaki</div></a></li>
+<li><a href="header-side-left.html"><div>Áo Khoác Vest Blazer</div></a></li>
+<li><a href="header-side-right.html"><div>Áo Khoác Lông</div></a></li>
+<li><a href="header-floating.html"><div>Áo Khoác Khác</div></a></li>
+</ul>
+</li>
+<li><a href="side-panel.html"><div><i class="icon-line-layout"></i>QUẦN – CHÂN VÁY</div></a>
+<ul>
+<li><a href="side-panel-left-overlay.html"><div>Chân Váy</div></a></li>
+<li><a href="side-panel-left-push.html"><div>Quần Nữ</div></a></li>
+<li><a href="side-panel-right-overlay.html"><div>Quần Jean Nữ</div></a></li>
+</ul>
+</li>
+<li><a href="#"><div><i class="icon-align-justify2"></i>KHÁC</div></a>
+<ul>
+<li><a href="header-light.html"><div>Bộ Đồ Thể Thao</div></a></li>
+<li><a href="menu-2.html"><div>Đồ Bơi Bikini</div></a></li>
+</ul>
+</li>
+<li><a href="profile.html"><div><i class="icon-user"></i>User Profile</div></a></li>
+</ul>
+</li>
+<li class="mega-menu"><a href="#"><div>Cosmetics</div></a>
+<div class="mega-menu-content style-2 clearfix">
+<ul class="mega-menu-column col-5">
+<li class="mega-menu-title"><a href="#"><div>Grids</div></a>
+<ul>
+<li><a href="portfolio-1.html"><div>1 Column</div></a></li>
+<li><a href="portfolio-2.html"><div>2 Columns</div></a></li>
+<li><a href="portfolio-3.html"><div>3 Columns</div></a></li>
+<li><a href="portfolio.html"><div>4 Columns</div></a></li>
+<li><a href="portfolio-5.html"><div>5 Columns</div></a></li>
+<li><a href="portfolio-6.html"><div>6 Columns</div></a></li>
+</ul>
+</li>
+</ul>
+<ul class="mega-menu-column col-5">
+<li class="mega-menu-title"><a href="#"><div>Masonry</div></a>
+<ul>
+<li><a href="portfolio-mixed-masonry.html"><div>Mixed Columns</div></a></li>
+<li><a href="portfolio-2-masonry.html"><div>2 Columns</div></a></li>
+<li><a href="portfolio-3-masonry.html"><div>3 Columns</div></a></li>
+<li><a href="portfolio-masonry.html"><div>4 Columns</div></a></li>
+<li><a href="portfolio-5-masonry.html"><div>5 Columns</div></a></li>
+<li><a href="portfolio-6-masonry.html"><div>6 Columns</div></a></li>
+</ul>
+</li>
+</ul>
+<ul class="mega-menu-column col-5">
+<li class="mega-menu-title"><a href="#"><div>Loading Styles</div></a>
+<ul>
+<li><a href="portfolio.html"><div>jQuery Filter</div></a></li>
+<li><a href="portfolio-jpagination.html"><div>jQuery Pagination</div></a></li>
+<li><a href="portfolio-infinity-scroll.html"><div>Infinity Scroll</div></a></li>
+<li><a href="portfolio-ajax.html"><div>AJAX In Page</div></a></li>
+<li><a href="portfolio-ajax-in-modal.html"><div>AJAX In Modal</div></a></li>
+<li><a href="portfolio-filter-styles.html"><div>Filter Styles</div></a></li>
+</ul>
+</li>
+ </ul>
+<ul class="mega-menu-column col-5">
+<li class="mega-menu-title"><a href="#"><div>Single Project</div></a>
+<ul>
+<li><a href="portfolio-single-extended.html"><div>Extended Item</div></a></li>
+<li><a href="portfolio-single-fullwidth.html"><div>Parallax Image</div></a></li>
+<li><a href="portfolio-single-gallery-full.html"><div>Slider Gallery</div></a></li>
+<li><a href="portfolio-single-video-fullwidth-left-sidebar.html"><div>HTML5 Video</div></a></li>
+<li><a href="portfolio-single-thumbs-right-sidebar.html"><div>Masonry Thumbs</div></a></li>
+<li><a href="portfolio-single-video-both-sidebar.html"><div>Embed Video</div></a></li>
+</ul>
+</li>
+</ul>
+<ul class="mega-menu-column col-5">
+<li class="mega-menu-title"><a href="#"><div>Layouts</div></a>
+<ul>
+<li><a href="portfolio-nomargin.html"><div>Default</div></a></li>
+<li><a href="portfolio-1-alt-right-sidebar.html"><div>Right Sidebar</div></a></li>
+<li><a href="portfolio-3-left-sidebar.html"><div>Left Sidebar</div></a></li>
+<li><a href="portfolio-2-both-sidebar.html"><div>Both Sidebar</div></a></li>
+<li><a href="portfolio-fullwidth-notitle.html"><div>100% Width</div></a></li>
+<li><a href="portfolio-parallax.html"><div>Parallax</div></a></li>
+</ul>
+</li>
+</ul>
+</div>
+</li>
+<li class="current"><a href="#"><div>Accessories</div></a>
+    <ul>
+        <li><a href="#">
+                <div><i class="icon-stack"></i>Váy đầm </div>
+            </a></li>
+        <li><a href="widgets.html">
+                <div><i class="icon-gift"></i>Áo</div>
+            </a>
+            <ul>
+                <li><a href="widgets.html">
+                        <div>Áo Sơ Mi Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Len Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Cardigan Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Thun Nữ</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="#">
+                <div><i class="icon-umbrella"></i>ÁO KHOÁC NỮ</div>
+            </a>
+            <ul>
+                <li><a href="header-light.html">
+                        <div>Áo Khoác Dạ</div>
+                    </a></li>
+                <li><a href="header-dark.html">
+                        <div>Áo Khoác Phao/Lông Vũ</div>
+                    </a></li>
+                <li><a href="header-transparent.html">
+                        <div>Áo Khoác Da</div>
+                    </a></li>
+                <li><a href="header-semi-transparent.html">
+                        <div>Áo Khoác Kaki</div>
+                    </a></li>
+                <li><a href="header-side-left.html">
+                        <div>Áo Khoác Vest Blazer</div>
+                    </a></li>
+                <li><a href="header-side-right.html">
+                        <div>Áo Khoác Lông</div>
+                    </a></li>
+                <li><a href="header-floating.html">
+                        <div>Áo Khoác Khác</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="side-panel.html">
+                <div><i class="icon-line-layout"></i>QUẦN – CHÂN VÁY</div>
+            </a>
+            <ul>
+                <li><a href="side-panel-left-overlay.html">
+                        <div>Chân Váy</div>
+                    </a></li>
+                <li><a href="side-panel-left-push.html">
+                        <div>Quần Nữ</div>
+                    </a></li>
+                <li><a href="side-panel-right-overlay.html">
+                        <div>Quần Jean Nữ</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="#">
+                <div><i class="icon-align-justify2"></i>KHÁC</div>
+            </a>
+            <ul>
+                <li><a href="header-light.html">
+                        <div>Bộ Đồ Thể Thao</div>
+                    </a></li>
+                <li><a href="menu-2.html">
+                        <div>Đồ Bơi Bikini</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="profile.html">
+                <div><i class="icon-user"></i>User Profile</div>
+            </a></li>
+    </ul>
+</li>
+<li class="current"><a href="#"><div>Women's shoes</div></a>
+    <ul>
+        <li><a href="#">
+                <div><i class="icon-stack"></i>Váy đầm </div>
+            </a></li>
+        <li><a href="widgets.html">
+                <div><i class="icon-gift"></i>Áo</div>
+            </a>
+            <ul>
+                <li><a href="widgets.html">
+                        <div>Áo Sơ Mi Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Len Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Cardigan Nữ</div>
+                    </a></li>
+                <li><a href="widgets.html">
+                        <div>Áo Thun Nữ</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="#">
+                <div><i class="icon-umbrella"></i>ÁO KHOÁC NỮ</div>
+            </a>
+            <ul>
+                <li><a href="header-light.html">
+                        <div>Áo Khoác Dạ</div>
+                    </a></li>
+                <li><a href="header-dark.html">
+                        <div>Áo Khoác Phao/Lông Vũ</div>
+                    </a></li>
+                <li><a href="header-transparent.html">
+                        <div>Áo Khoác Da</div>
+                    </a></li>
+                <li><a href="header-semi-transparent.html">
+                        <div>Áo Khoác Kaki</div>
+                    </a></li>
+                <li><a href="header-side-left.html">
+                        <div>Áo Khoác Vest Blazer</div>
+                    </a></li>
+                <li><a href="header-side-right.html">
+                        <div>Áo Khoác Lông</div>
+                    </a></li>
+                <li><a href="header-floating.html">
+                        <div>Áo Khoác Khác</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="side-panel.html">
+                <div><i class="icon-line-layout"></i>QUẦN – CHÂN VÁY</div>
+            </a>
+            <ul>
+                <li><a href="side-panel-left-overlay.html">
+                        <div>Chân Váy</div>
+                    </a></li>
+                <li><a href="side-panel-left-push.html">
+                        <div>Quần Nữ</div>
+                    </a></li>
+                <li><a href="side-panel-right-overlay.html">
+                        <div>Quần Jean Nữ</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="#">
+                <div><i class="icon-align-justify2"></i>KHÁC</div>
+            </a>
+            <ul>
+                <li><a href="header-light.html">
+                        <div>Bộ Đồ Thể Thao</div>
+                    </a></li>
+                <li><a href="menu-2.html">
+                        <div>Đồ Bơi Bikini</div>
+                    </a></li>
+            </ul>
+        </li>
+        <li><a href="profile.html">
+                <div><i class="icon-user"></i>User Profile</div>
+            </a></li>
+    </ul>
+</li>
+@guest
+<li class="current"><a href="{{ route('login') }}"><div>Login</div></a></li>
+@if (Route::has('register'))
+<li class="current"><a href="{{ route('register') }}"><div>Register</div></a></li>
+@endif
+@else
+<li class="current"><a href="#">
+        <div>{{ Auth::user()->name }}</div>
+    </a>
+    <ul>
+        <li><a href="profile.html">
+                <div><i class="icon-user"></i>User Profile</div>
+            </a></li>
+        <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();" >
+                <div><i class="icon-line-layout"></i>Log out</div>
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </li>
+    </ul>
+</li>
+@endguest
+</ul>
+
+<div id="top-cart">
+<a href="#" id="top-cart-trigger"><i class="icon-shopping-cart"></i><span>5</span></a>
+<div class="top-cart-content">
+<div class="top-cart-title">
+<h4>Shopping Cart</h4>
+</div>
+<div class="top-cart-items">
+<div class="top-cart-item clearfix">
+<div class="top-cart-item-image">
+<a href="#"><img src="images/shop/small/1.jpg" alt="Blue Round-Neck Tshirt" /></a>
+</div>
+<div class="top-cart-item-desc">
+<a href="#">Blue Round-Neck Tshirt</a>
+<span class="top-cart-item-price">$19.99</span>
+<span class="top-cart-item-quantity">x 2</span>
+</div>
+</div>
+<div class="top-cart-item clearfix">
+<div class="top-cart-item-image">
+<a href="#"><img src="images/shop/small/6.jpg" alt="Light Blue Denim Dress" /></a>
+</div>
+<div class="top-cart-item-desc">
+<a href="#">Light Blue Denim Dress</a>
+<span class="top-cart-item-price">$24.99</span>
+<span class="top-cart-item-quantity">x 3</span>
+</div>
+</div>
+</div>
+<div class="top-cart-action clearfix">
+<span class="fleft top-checkout-price">$114.95</span>
+<button class="button button-3d button-small nomargin fright">View Cart</button>
+</div>
+</div>
+</div>
+
+<div id="top-search">
+<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+<form action="http://themes.semicolonweb.com/html/canvas/search.html" method="get">
+<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+</form>
+</div>
+</nav>
+</div>
+</div>
+</header>
